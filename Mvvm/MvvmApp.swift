8ae9a752228372f6b15f005937c3fb6c705f5be7
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2023-01-14 08:41:43 +0300

    Initial Commit

---
+++ Mvvm/MvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MvvmApp.swift
+//  Mvvm
+//
+//  Created by Ian Nalyanya on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
